aea9db3c5cc9459c85abc8595409f2939be6afd6
Otávio Sales <otavio.sales@meuvarejo360.com.br>
2021-09-28 19:29:20 -0300

    Commit automatico

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 ################ IMPORTS ######################
 from flask import Flask, render_template, request, jsonify
+from flask.helpers import url_for
 import joblib
 import nltk
 import re
@@ -7,17 +8,13 @@ import json
 import twint
 import pandas as pd
 import matplotlib.pyplot as plt
-import datetime
+import random
+import time
 import os
 from celery import Celery
 
 
 def make_celery(app):
-    # celery = Celery(
-    #     app.import_name,
-    #     backend=app.config['CELERY_RESULT_BACKEND'],
-    #     broker=app.config['CELERY_BROKER_URL']
-    # )
     broker = os.environ['REDIS_URL']
     backend = os.environ['REDIS_URL']
     name = os.environ.get('CELERY_NAME', 'default_name')
@@ -43,14 +40,61 @@ model = joblib.load('brimo_model.pkl')
 
 celery = make_celery(app)
 
-@celery.task()
-def add_together(a, b):
-    return a + b
+@celery.task(bind=True)
+def long_task(self):
+    """Background task that runs a long function with progress reports."""
+    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
+    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+    message = ''
+    total = random.randint(10, 50)
+    for i in range(total):
+        if not message or random.random() < 0.25:
+            message = '{0} {1} {2}...'.format(random.choice(verb),
+                                              random.choice(adjective),
+                                              random.choice(noun))
+        self.update_state(state='PROGRESS',
+                          meta={'current': i, 'total': total,
+                                'status': message})
+        time.sleep(1)
+    return {'current': 100, 'total': 100, 'status': 'Task completed!',
+            'result': 42}
 
-@app.route('/test')
-def test():
-    result = add_together.delay(23, 42)
-    result.wait()
+@app.route('/longtask', methods=['POST'])
+def longtask():
+    task = long_task.apply_async()
+    return jsonify({}), 202, {'Location': url_for('taskstatus',
+                                                  task_id=task.id)}
+
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        # job did not start yet
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
 ################# FLASK API ####################
 #default page of our web-app
 @app.route('/')
